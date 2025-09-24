940a323be3b4cb62f7b863344aa4750a52ba24c8
Jeferson Alan <cabello_10@live.com>
2025-09-24 15:25:17 -0300

    Add files via upload

---
+++ Cap_4/questionario.py
@@ -0,0 +1,5 @@
+nome = input("Qual seu nome? ")
+idade = input("Qual sua idade? ")
+hobby = input("Qual seu hobby favorito? ")
+
+print("Prazer,", nome, "! Você tem", idade, "anos e seu hobby predileto é", hobby, ".")
